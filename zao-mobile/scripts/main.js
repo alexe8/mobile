4ef6445978a63eeea5a1b404782864ecbc36c425
du dom <dudom8@gmail.com>
2013-10-06 12:24:38 -0500

    Changes made by du dom (dudom8@gmail.com).

--- zao-mobile/scripts/main.js
+++ zao-mobile/scripts/main.js
@@ -4,6 +4,7 @@ document.addEventListener("deviceready", onDeviceReady, false);
 
 // PhoneGap is ready
 function onDeviceReady() {
+     
     
 	
 }
@@ -11,67 +12,87 @@ function onDeviceReady() {
 var zaoApp = function(){}
 
 zaoApp.prototype = function() {
-    console.log("T2");
-    //var _flightForCheckin = null,
-    //_flightForDetails=null,
-    //_ffNum = null, 
-    //_customerData = null,
+    
+    
     _login = false,
     
     run = function(){
-      
-        var that = this,
-        $seatPicker=$('#seatPicker');
-        $('#tripDetail').on('pagebeforeshow',$.proxy(_initTripDetail,that));
-        $('#boardingPass').on('pageshow',$.proxy(_initBoardingPass,that));
-        $('#home').on('pagebeforecreate',$.proxy(_initHome,that));
-        $('#checkIn').on('pageshow', $.proxy(_initCheckIn,that));
         
-        $('#myTripsListView').on('click', 'li', function () {
-        	var item = $(this);
-        	_flightForCheckin = item.data('flight');
-            _flightForDetails = item.data('flight');
+      /*$('#login').click(function ()
+        {   
+            console.log("Start");
+            $.ajax({
+                type: "post",
+                url: "https://www.zao.com//referrer/create", 
+                data: {},
+                success: function(msg){      
+                        console.log(msg);
+                }
+            });
+        });*/
+
+        // variable to hold request
+        var request;
+        // bind to the submit event of our form
+        $("#logInForm").submit(function(event){
+            // abort any pending request
+            if (request) {
+                request.abort();
+            }
+            // setup some local variables
+            var $form = $(this);
+            // let's select and cache all the fields
+            var $inputs = $form.find("input, select, button, textarea");
+            // serialize the data in the form
+            var serializedData = $form.serialize();
+            // let's disable the inputs for the duration of the ajax request
+            $inputs.prop("disabled", true);
+            // fire off the request to url
+            request = $.ajax({
+                url: "http://dev.zao.com//referrer/create",
+                type: "post",
+                data: serializedData
+            });
+
+            // callback handler that will be called on success
+            request.done(function (response, textStatus, jqXHR){
+                // log a message to the console
+                console.log("Hooray, it worked!");
+                console.log("M:Response:"+response);
+                console.log("M:TextStatus:"+textStatus);
+                console.log("M:JqXHR:"+jqXHR);
+            });
+
+            // callback handler that will be called on failure
+            request.fail(function (jqXHR, textStatus, errorThrown){
+                // log the error to the console
+                console.error(
+                    "The following error occured: "+
+                    textStatus, errorThrown
+                );
+            });
+
+            // callback handler that will be called regardless
+            // if the request failed or succeeded
+            request.always(function () {
+                // reenable the inputs
+                $inputs.prop("disabled", false);
+            });
+
+            // prevent default posting of form
+            event.preventDefault();
         });
         
-        $seatPicker.on('pageshow', function (event) {
-        	var el = $('#seatMapPickerContainer', this),
-        	seat = _flightForCheckin.segments[_flightForCheckin.currentSegment].seat;
-        	seatMapDrawing.drawSeatMap(el, seat);
         
-        });
-        
-        $seatPicker.on('pagebeforehide', function (event) {
-        	_flightForCheckin.segments[_flightForCheckin.currentSegment].seat = seatMapDrawing.getselectedSeat();
-        });
-    },
+    };
     
     
     
-    _initHome = function(){
-        if (!_login) {
-            console.log("r");
-	    	$.mobile.changePage("#logIn", { transition: "flip" });
-	    	$('#login').submit(function () {
-	    		$(this).hide();
-	    		_login = true;
-	    		airData.logOn($('#userName').val(), $('#pwd').val(),_handleLogOn);
-	    		return false;
-	    	});
-	    }
-    },
     
-    
-    
-    _handleLogOn = function (ff, success) {
-		if (success) {
-			_ffNum = ff;
-            console.log("t");
-			airData.getDataforFF(_ffNum,_handleDataForFF);
-		}
-	};
    
     
     return {
         run:run,
     };
 }();
+
